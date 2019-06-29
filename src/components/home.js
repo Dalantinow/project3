eba2c8b05504afc49388733df2b182b029e8c69a
Bonesaw <dalantinow@gmail.com>
2019-06-29 11:49:22 -0400

    I have a basic login form and mongo database attached, but the post to the database will not function

---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    constructor() {
+        super()
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>USE NAVBAR</p>
+            </div>
+        )
+
+    }
+}
+
+export default Home
